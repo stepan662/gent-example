d2cc7f279323455e7fcecbe5c2ab9aa9b0d15f21
stepan <stepan.granat@kiwi.com>
2020-05-10 17:52:46 +0200

    gent-core update

--- process/src/modifiers/CustomModifierInterface.ts
+++ process/src/modifiers/CustomModifierInterface.ts
@@ -1,5 +1,6 @@
-import { ModifierType, ProcessStateType } from 'gent-core/lib/Types'
+import { ModifierType, ProcessStateType, JournalMutationType } from 'gent-core/lib/Types'
 
 export default interface CustomModifierInterface extends ModifierType {
   getProcesses(): Promise<ProcessStateType[]>
+  getJournalEntries(processId: string): Promise<JournalMutationType[]>
 }
--- process/src/modifiers/localModifier.ts
+++ process/src/modifiers/localModifier.ts
@@ -1,7 +1,14 @@
 import LocalModifier from 'gent-core/lib/tools/LocalModifier'
 import CustomModifierInterface from './CustomModifierInterface'
+import { JournalMutationType } from 'gent-core/lib/Types'
 
 class Modifier extends LocalModifier implements CustomModifierInterface {
+  async getJournalEntries(processId: string): Promise<JournalMutationType[]> {
+    const result = this.data.journal.filter((mutation) => mutation.process_id === processId)
+    result.reverse()
+    return result
+  }
+
   async getProcesses() {
     const result = [...this.data.process]
     result.reverse()
